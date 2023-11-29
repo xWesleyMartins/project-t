9f7e755d98e2957982dee113b9c7df90e89b9979
xWesleyMartins <wesley_869@hotmail.com.br>
2023-11-28 23:42:10 -0300

    Adicionado configuração do "dotenv"

--- src/Database/database.providers.ts
+++ src/Database/database.providers.ts
@@ -1,16 +1,18 @@
 import { DataSource } from 'typeorm';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'tauron',
-        password: '1234',
-        database: 'basic_crud_DB',
+        host: process.env.DB_HOST,
+        port: parseInt(process.env.DB_PORT),
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });
